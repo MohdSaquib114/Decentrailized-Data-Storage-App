a1158cbb4bc3fa1bc1c4d141fb77da3d912b2733
MohdSaquib114 <msmsaq114@gmail.com>
2025-04-25 07:19:08 +0530

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [  tailwindcss(),react()],  assetsInclude: ["**/*.glb"],
+  server: {
+    historyApiFallback: true
+  },
+  preview: {
+    historyApiFallback: true
+  }
 })
+
+
